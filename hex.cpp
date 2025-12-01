a852c37d9758c95387040fe1d8cb18c870ea9640
Alexandr <91843634+BelotelovAV@users.noreply.github.com>
2025-12-01 15:59:48 +0300

    Add files via upload

--- hex.cpp
+++ hex.cpp
@@ -18,4 +18,31 @@ int main(){
         if (!cin|n<0)
             break;
         int2txt(int n, int d, txt, 256);
-        
+        cout << "n= " << txt << endl;
+    }
+    return 0;
+}
+
+void int2txt(int n, int d, char *txt, int maxLen){
+    if (n == 0) {
+        txt[0] = '0';
+        txt[1] = 0;
+        return;
+    }
+    assert(n > 0);
+    int k = 0;
+    while (n>0 && k < maxLen-1){
+        int c = n%d;
+        txt[k] = DIGITS[c];
+        ++k;
+    }
+    txt[k] = 0;
+    
+    //invert the text
+    
+    for (int i = 0, j = k-1; i<j; ++i, --j){
+        char tmp = txt[i];
+        txt[i] = txt[j];
+        txt[j] = tmp;
+    }
+}
