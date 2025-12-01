948e4c925239706c3f97519d9c84ee787d1d5769
Alexandr <91843634+BelotelovAV@users.noreply.github.com>
2025-12-01 16:05:45 +0300

    Add files via upload

--- hex.cpp
+++ hex.cpp
@@ -5,9 +5,8 @@ using namespace std;
 
 constexpr const char * DIGITS = "0123456789ABCDEF";
 constexpr int NUM_DIGITS = 16;
-int d = 16;
 
-void int2txt(int n, int d, char *txt, int maxLen);
+void int2txt(int n, char *txt, int maxLen);
 
 int main(){
     char txt[256];
@@ -17,13 +16,13 @@ int main(){
         cin >> n;
         if (!cin|n<0)
             break;
-        int2txt(int n, int d, txt, 256);
+        int2txt(n, txt, 256);
         cout << "n= " << txt << endl;
     }
     return 0;
 }
 
-void int2txt(int n, int d, char *txt, int maxLen){
+void int2txt(int n, char *txt, int maxLen){
     if (n == 0) {
         txt[0] = '0';
         txt[1] = 0;
@@ -32,7 +31,8 @@ void int2txt(int n, int d, char *txt, int maxLen){
     assert(n > 0);
     int k = 0;
     while (n>0 && k < maxLen-1){
-        int c = n%d;
+        int c = n%16;
+        n /= 16;
         txt[k] = DIGITS[c];
         ++k;
     }
